73213f5f66e1e749f8bfcfae7aaef87e684149e5
klapierre <lapierre.kimberly@gmail.com>
2016-07-07 16:53:07 -0700

    Set up R script to calculate mean community change from pre-treatment year.

---
+++ nutnet_mean community change.R
@@ -0,0 +1,12 @@
+library(lattice)
+library(nlme)
+library(vegan)
+library(plyr)
+library(dplyr)
+library(tidyr)
+
+
+setwd('C:\\Users\\Kim\\Dropbox\\working groups\\HRF response - NutNet and CORRE\\NutNet data')
+
+source('C:\\Users\\Kim\\Dropbox\\working groups\\HRF response - NutNet and CORRE\\HRF-test\\nutnet-cover-data.R')
+

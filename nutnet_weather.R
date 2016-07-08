47fcdd225db5f427e3cbc88f7e3844a62464fd66
klapierre <lapierre.kimberly@gmail.com>
2016-07-07 18:13:54 -0700

    Get weather data for Konza.

---
+++ nutnet_weather.R
@@ -0,0 +1,11 @@
+library(lattice)
+library(nlme)
+library(vegan)
+library(plyr)
+library(dplyr)
+library(tidyr)
+
+
+setwd('C:\\Users\\Kim\\Dropbox\\working groups\\HRF response - NutNet and CORRE\\NutNet data')
+
+nutnetWeather <- read.csv('konza_precip_2007-2012.csv')

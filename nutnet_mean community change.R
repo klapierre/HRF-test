d0a4be118664bdbad496fc0435fbcad2b306c41b
klapierre <lapierre.kimberly@gmail.com>
2016-07-08 12:26:50 -0700

    Add Mt Caroline data.

--- nutnet_mean community change.R
+++ nutnet_mean community change.R
@@ -145,3 +145,48 @@ for(i in 1:length(plotList$plot)) {
   cdrBC <- rbind(dissimilarity, cdrBC)
   
 }
+
+
+
+
+#test on just mt carolina data
+mtcaCover <- subset(nutnetRelCover, site_code=='mtca.au')%>%
+  #transpose
+  spread(key=Taxon, value=rel_cover, fill=0)
+
+#create a dataframe with plot numbers
+plotList <- data.frame(plot=unique(mtcaCover$plot))
+
+#create an empty dataframe to collect dissimilarity values
+mtcaBC <- data.frame(row.names=1)
+
+#for each plot, calculate bray-curtis dissimilarity across all years
+for(i in 1:length(plotList$plot)) {
+  
+  #get site cover data
+  dataset=mtcaCover[mtcaCover$plot==as.character(plotList$plot[i]),]
+  
+  #calculate bray-curtis dissimilarity
+  BC <- as.data.frame(as.matrix(vegdist(dataset[,6:68], method='bray', diag=F, upper=T)))
+  
+  #keep just first column of BC dissimilarity matrix
+  BC1 <- as.data.frame(BC[,1])
+  names(BC1)[names(BC1)=='BC[, 1]'] <- 'dissimilarity'
+  
+  #gather years of data
+  years <- as.data.frame(dataset$year)
+  names(years)[names(years)=='dataset$year'] <- 'year'
+  years <- years%>%
+    mutate(yr0=min(year))%>%
+    mutate(BC_years=paste(yr0, year, sep='::'))%>%
+    select(BC_years, year)
+  
+  #bind year labels to dissimilarity data
+  dissimilarity <- cbind(BC1, years)%>%
+    #add plot label
+    mutate(plot=plotList$plot[i])
+  
+  #paste all data into dataframe made for the analysis
+  mtcaBC <- rbind(dissimilarity, mtcaBC)
+  
+}

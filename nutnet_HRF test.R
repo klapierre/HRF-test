07325a42dcb822ef5fbbc7215e5034210cdccd8d
klapierre <lapierre.kimberly@gmail.com>
2016-07-07 17:59:28 -0700

    Plot dissimilarity vs anpp.

--- nutnet_HRF test.R
+++ nutnet_HRF test.R
@@ -1,6 +1,7 @@
 library(lattice)
 library(nlme)
 library(vegan)
+library(ggplot2)
 library(plyr)
 library(dplyr)
 library(tidyr)
@@ -9,4 +10,15 @@ library(tidyr)
 setwd('C:\\Users\\Kim\\Dropbox\\working groups\\HRF response - NutNet and CORRE\\NutNet data')
 
 source('C:\\Users\\Kim\\Dropbox\\working groups\\HRF response - NutNet and CORRE\\HRF-test\\nutnet-basic-data-script.R')
-source('C:\\Users\\Kim\\Dropbox\\working groups\\HRF response - NutNet and CORRE\\HRF-test\\nutnet-cover-data.R')
+source('C:\\Users\\Kim\\Dropbox\\working groups\\HRF response - NutNet and CORRE\\HRF-test\\nutnet_mean community change.R')
+
+#test on just Konza data
+#subset out Konza biomass data
+konzaBio <- subset(trtBio, site_code=='konz.us')
+
+#merge biomass and mean change data
+konza <- merge(konzaBC, konzaBio, by=c('year', 'plot'))
+
+#plot
+ggplot(data=subset(konza, trt=='NPK'), aes(x=dissimilarity, y=live_mass_diff, color=year)) +
+  geom_point()

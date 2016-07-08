491e8fe84f59afad07ed807dd7b0df08e1327bff
klapierre <lapierre.kimberly@gmail.com>
2016-07-07 17:42:49 -0700

    Using Konza as an example, calculate bray-curtis dissimilarity for each plot compared to pre-treatment year.

--- nutnet_mean community change.R
+++ nutnet_mean community change.R
@@ -10,3 +10,42 @@ setwd('C:\\Users\\Kim\\Dropbox\\working groups\\HRF response - NutNet and CORRE\
 
 source('C:\\Users\\Kim\\Dropbox\\working groups\\HRF response - NutNet and CORRE\\HRF-test\\nutnet-cover-data.R')
 
+#test on just Konza data
+konzaCover <- subset(nutnetRelCover, site_code=='konz.us')%>%
+  #transpose
+  spread(key=Taxon, value=rel_cover, fill=0)
+
+#create a dataframe with plot numbers
+plotList <- data.frame(plot=unique(konzaCover$plot))
+
+#create an empty dataframe to collect dissimilarity values
+konzaBC <- data.frame(row.names=1)
+
+#for each plot, calculate bray-curtis dissimilarity across all years
+for(i in 1:length(plotList$plot)) {
+  
+  #get site cover data
+  dataset=konzaCover[konzaCover$plot==as.character(plotList$plot[i]),]
+  
+  #calculate bray-curtis dissimilarity
+  BC <- as.data.frame(as.matrix(vegdist(dataset[,6:83], method='bray', diag=F, upper=T)))
+  
+  #keep just first column of BC dissimilarity matrix
+  BC1 <- as.data.frame(BC[,1])
+  names(BC1)[names(BC1)=='BC[, 1]'] <- 'dissimilarity'
+  
+  #gather years of data
+  years <- as.data.frame(dataset$year)%>%
+    mutate(yr0=min(dataset$year))%>%
+    mutate(BC_years=paste(yr0, dataset$year, sep='::'))%>%
+    select(BC_years)
+  
+  #bind year labels to dissimilarity data
+  dissimilarity <- cbind(BC1, years)%>%
+    #add plot label
+    mutate(plot=plotList$plot[i])
+  
+  #paste all data into dataframe made for the analysis
+  konzaBC <- rbind(dissimilarity, konzaBC)
+  
+}

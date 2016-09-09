aee9139b694c76eb34e8d43a5dabad586c5f85c5
klapierre <lapierre.kimberly@gmail.com>
2016-09-09 10:48:26 -0700

    Change figures to include dot size as variable.

--- nutnet_HRF test.R
+++ nutnet_HRF test.R
@@ -2,6 +2,7 @@ library(lattice)
 library(nlme)
 library(vegan)
 library(ggplot2)
+library(grid)
 library(plyr)
 library(dplyr)
 library(tidyr)
@@ -11,11 +12,31 @@ setwd('C:\\Users\\Kim\\Dropbox\\working groups\\HRF response - NutNet and CORRE\
 
 
 theme_set(theme_bw())
-theme_update(axis.title.x=element_text(size=40, vjust=-0.35, margin=margin(t=15)), axis.text.x=element_text(size=34),
-             axis.title.y=element_text(size=40, angle=90, vjust=0.5, margin=margin(r=15)), axis.text.y=element_text(size=34),
+theme_update(axis.title.x=element_text(size=35, vjust=-0.35, margin=margin(t=15)), axis.text.x=element_text(size=30),
+             axis.title.y=element_text(size=35, angle=90, vjust=0.5, margin=margin(r=15)), axis.text.y=element_text(size=30),
              plot.title = element_text(size=24, vjust=2),
              panel.grid.major=element_blank(), panel.grid.minor=element_blank(),
              legend.title=element_blank(), legend.text=element_text(size=20))
+
+
+###bar graph summary statistics function
+#barGraphStats(data=, variable="", byFactorNames=c(""))
+barGraphStats <- function(data, variable, byFactorNames) {
+  count <- length(byFactorNames)
+  N <- aggregate(data[[variable]], data[byFactorNames], FUN=length)
+  names(N)[1:count] <- byFactorNames
+  names(N) <- sub("^x$", "N", names(N))
+  mean <- aggregate(data[[variable]], data[byFactorNames], FUN=mean)
+  names(mean)[1:count] <- byFactorNames
+  names(mean) <- sub("^x$", "mean", names(mean))
+  sd <- aggregate(data[[variable]], data[byFactorNames], FUN=sd)
+  names(sd)[1:count] <- byFactorNames
+  names(sd) <- sub("^x$", "sd", names(sd))
+  preSummaryStats <- merge(N, mean, by=byFactorNames)
+  finalSummaryStats <- merge(preSummaryStats, sd, by=byFactorNames)
+  finalSummaryStats$se <- finalSummaryStats$sd / sqrt(finalSummaryStats$N)
+  return(finalSummaryStats)
+}  
 
 
 source('C:\\Users\\Kim\\Dropbox\\working groups\\HRF response - NutNet and CORRE\\HRF-test\\nutnet-basic-data-script.R')
@@ -26,18 +47,26 @@ source('C:\\Users\\Kim\\Dropbox\\working groups\\HRF response - NutNet and CORRE
 konzaBio <- subset(trtBio, site_code=='konz.us')
 
 #merge biomass and mean change data
-konza <- merge(konzaBC, konzaBio, by=c('year', 'plot'))
+konza <- merge(konzaBC, konzaBio, by=c('year', 'plot'))%>%
+  group_by(year, trt)%>%
+  summarise(live_mass_diff=mean(live_mass_diff), dissimilarity=mean(dissimilarity))
 
 #plot dissimilarity vs biomass change
 ggplot(data=subset(konza, trt=='NPK'), aes(x=dissimilarity, y=live_mass_diff, color=year)) +
   geom_point(size=6)
 
 #plot year vs biomass change
-ggplot(data=subset(konza, trt=='NPK'), aes(x=year, y=live_mass_diff, color=dissimilarity)) +
-  geom_point(size=6) +
-  scale_color_gradient(low='#330066', high='#FF0000') +
+konzaBioPlot <- ggplot(data=subset(konza, trt=='NPK'), aes(x=year, y=live_mass_diff, color=dissimilarity)) +
+  geom_point(aes(size=dissimilarity)) +
+  scale_color_gradient(name='Community\nDissimilarity', low='#330066', high='#FF0000', limits=c(0,1)) +
+  scale_size_continuous(range=c(6,20)) +
+  scale_x_continuous(breaks=seq(2008,2014,2), limits=c(2008,2015)) +
+  scale_y_continuous(breaks=seq(-1,3,1), limits=c(-1,3)) +
   xlab('Year') +
-  ylab('Live Biomass Response Ratio')
+  ylab('Live Biomass Response Ratio') +
+  annotate('text', x=2008, y=3, label='(a) Konza Prairie', size=10, hjust='left') +
+  theme(legend.title=element_text(size=20, vjust='center', margin=margin(b=50)), legend.position=c(0.8,0.7)) +
+  guides(color=F)
 
 #plot year vs dissimilarity
 ggplot(data=subset(konza, trt=='NPK'), aes(x=year, y=dissimilarity, color=live_mass_diff)) +
@@ -51,7 +80,79 @@ ggplot(data=subset(konza, trt=='NPK'), aes(x=year, y=dissimilarity, color=live_m
 
 
 
+#test on just smith prairie data
+#subset out smith biomass data
+smithBio <- subset(trtBio, site_code=='smith.us')
+
+#merge biomass and mean change data
+smith <- merge(smithBC, smithBio, by=c('year', 'plot'))%>%
+  group_by(year, trt)%>%
+  summarise(live_mass_diff=mean(live_mass_diff), dissimilarity=mean(dissimilarity))
+
+#plot dissimilarity vs biomass change
+ggplot(data=subset(smith, trt=='NPK'), aes(x=dissimilarity, y=live_mass_diff, color=year)) +
+  geom_point(size=6)
+
+#plot year vs biomass change
+smithBioPlot <- ggplot(data=subset(smith, trt=='NPK'), aes(x=year, y=live_mass_diff, color=dissimilarity)) +
+  geom_point(aes(size=dissimilarity)) +
+  scale_size_continuous(range=c(6,20)) +
+  scale_color_gradient(name='Community\nDissimilarity', low='#330066', high='#FF0000', limits=c(0,1)) +
+  scale_x_continuous(breaks=seq(2008,2014,2), limits=c(2008,2015)) +
+  scale_y_continuous(breaks=seq(-1,3,1), limits=c(-1,3)) +
+  xlab('Year') +
+  ylab('') +
+  annotate('text', x=2008, y=3, label='(b) Smith Prairie', size=10, hjust='left') +
+  theme(legend.position='none')
+
+#plot year vs dissimilarity
+ggplot(data=subset(smith, trt=='NPK'), aes(x=year, y=dissimilarity, color=live_mass_diff)) +
+  geom_point(size=6) +
+  scale_color_gradient(name='Biomass\nResponse\nRatio', low='#330066', high='#FF0000') +
+  xlab('Year') +
+  ylab('Bray-Curtis Dissimilarity') +
+  theme(legend.title=element_text(size=20, vjust='center'))
 
 
 
 
+
+
+#test on just cedar creek data
+#subset out cedar creek biomass data
+cdrBio <- subset(trtBio, site_code=='cdcr.us')
+
+#merge biomass and mean change data
+cdr <- merge(cdrBC, cdrBio, by=c('year', 'plot'))%>%
+  group_by(year, trt)%>%
+  summarise(live_mass_diff=mean(live_mass_diff), dissimilarity=mean(dissimilarity))
+
+#plot dissimilarity vs biomass change
+ggplot(data=subset(cdr, trt=='NPK'), aes(x=dissimilarity, y=live_mass_diff, color=year)) +
+  geom_point(size=6)
+
+#plot year vs biomass change
+cdrBioPlot <- ggplot(data=subset(cdr, trt=='NPK'), aes(x=year, y=live_mass_diff, color=dissimilarity)) +
+  geom_point(aes(size=dissimilarity)) +
+  scale_size_continuous(range=c(6,20)) +
+  scale_color_gradient(name='Community\nDissimilarity', low='#330066', high='#FF0000', limits=c(0,1)) +
+  scale_y_continuous(breaks=seq(-1,3,1), limits=c(-1,3)) +
+  xlab('Year') +
+  ylab('') +
+  annotate('text', x=2008, y=3, label='(c) Cedar Creek', size=10, hjust='left') +
+  theme(legend.position='none')
+
+#plot year vs dissimilarity
+ggplot(data=subset(cdr, trt=='NPK'), aes(x=year, y=dissimilarity, color=live_mass_diff)) +
+  geom_point(size=6) +
+  scale_color_gradient(name='Biomass\nResponse\nRatio', low='#330066', high='#FF0000') +
+  xlab('Year') +
+  ylab('Bray-Curtis Dissimilarity') +
+  theme(legend.title=element_text(size=20, vjust='center'))
+
+
+pushViewport(viewport(layout=grid.layout(1,3)))
+print(konzaBioPlot, vp=viewport(layout.pos.row = 1, layout.pos.col = 1))
+print(smithBioPlot, vp=viewport(layout.pos.row = 1, layout.pos.col = 2))
+print(cdrBioPlot, vp=viewport(layout.pos.row = 1, layout.pos.col = 3))
+#export at 1600x800
--- nutnet_temporal community change.R
+++ nutnet_temporal community change.R
@@ -1,6 +1,7 @@
 library(lattice)
 library(nlme)
 library(vegan)
+library(codyn)
 library(plyr)
 library(dplyr)
 library(tidyr)
@@ -9,3 +10,43 @@ library(tidyr)
 setwd('C:\\Users\\Kim\\Dropbox\\working groups\\HRF response - NutNet and CORRE\\NutNet data')
 
 source('C:\\Users\\Kim\\Dropbox\\working groups\\HRF response - NutNet and CORRE\\HRF-test\\nutnet-cover-data.R')
+
+
+#test on just Konza data
+konzaCover <- subset(nutnetRelCover, site_code=='konz.us')
+
+#create a dataframe with plot numbers
+plotList <- data.frame(plot=unique(konzaCover$plot))
+
+#create an empty dataframe to collect dissimilarity values
+konzaTempChange <- data.frame(row.names=1)
+
+#for each plot, calculate bray-curtis dissimilarity across all years
+for(i in 1:length(plotList$plot)) {
+  
+  #get site cover data
+  dataset=konzaCover[konzaCover$plot==as.character(plotList$plot[i]),]%>%
+    #get just first and last year
+    filter(year==min(year)|year==max(year))
+  
+  #calculate disappearance
+  disappearance=turnover(df=dataset, time.var='year', species.var='Taxon', abundance.var='rel_cover', replicate.var=NA, metric='disappearance')%>%
+    group_by(year)%>%
+    summarise(disappearance=mean(disappearance))
+  
+  #calculate appearance
+  appearance=turnover(df=dataset, time.var='year', species.var='Taxon', abundance.var='rel_cover', replicate.var=NA, metric='appearance')%>%
+    group_by(year)%>%
+    summarise(appearance=mean(appearance))
+  
+  #merging back with labels to get back plot_mani
+  turnover=labels%>%
+    left_join(disappearance, by='calendar_year')%>%
+    left_join(appearance, by='calendar_year')%>%
+    filter(calendar_year==max(calendar_year))%>%
+    select(exp_trt, plot_mani, appearance, disappearance)
+  
+  #pasting variables into the dataframe made for this analysis
+  konzaTempChange=rbind(turnover, konzaTempChange)
+  
+}
